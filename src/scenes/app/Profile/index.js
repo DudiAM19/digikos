fa2750c087b3c47ea7a3ee8d9351b58331c7bce8
DudiAM19 <dudiali79@gmail.com>
2022-01-17 21:27:42 +0700

    profile

--- src/scenes/app/Profile/index.js
+++ src/scenes/app/Profile/index.js
@@ -1,13 +1,101 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import {Container} from 'components';
+import {View, Text, Image, TouchableOpacity, FlatList} from 'react-native';
+import {Container, Header, Icon} from 'components';
 import {Colors} from 'styles';
+import styles from './styles';
+
+const DATA = [
+  {
+    text: 'Pengaturan Akun',
+    icon: 'settings-outline',
+    icontype: 'Ionicons',
+    color: Colors.ORANGE,
+  },
+  {
+    text: 'Notifikasi',
+    icon: 'notifications-none',
+    icontype: 'MaterialIcons',
+    color: '#AC4DFE',
+  },
+  {
+    text: 'Bantuan',
+    icon: 'help-circle',
+    icontype: 'Feather',
+    color: '#1FB8FC',
+  },
+  {
+    text: 'Berikan Feedback',
+    icon: 'notification',
+    icontype: 'AntDesign',
+    color: '#EA307D',
+  },
+  {
+    text: 'Keluar',
+    icon: 'log-out',
+    icontype: 'Feather',
+    color: '#F9B403',
+  },
+];
+
+const Item = props => {
+  return (
+    <TouchableOpacity style={styles.fitursection}>
+      <Icon
+        name={props.icon}
+        type={props.icontype}
+        style={styles.iconfitur(props.color)}
+      />
+      <Text style={styles.textfitur}>{props.text}</Text>
+      <Icon name="chevron-right" type="Feather" style={styles.chevronright} />
+    </TouchableOpacity>
+  );
+};
 
 const Profile = ({navigation}) => {
   return (
-    <Container backroundColor={Colors.WHITE}>
-      <Text>Profile</Text>
+    <Container backgroundColor={Colors.WHITE}>
+      <Header lgTitle="Akun Saya" />
+      <View style={styles.container}>
+        <View style={styles.profilesection}>
+          <View style={styles.textprofile}>
+            <Text style={styles.textname}>Yudi Wahyudi</Text>
+            <Text style={styles.textowner}>Pemilik</Text>
+          </View>
+          <Image
+            source={require('../../../assets/images/User.png')}
+            style={styles.imgprofile}
+          />
+        </View>
+        <TouchableOpacity style={styles.premiummember}>
+          <Image
+            source={require('../../../assets/images/best-seller.png')}
+            style={styles.imgpremiummember}
+          />
+          <Text style={styles.textpremiummember}>Premium Member</Text>
+          <Icon
+            name="chevron-right"
+            type="Feather"
+            style={styles.chevronright}
+          />
+        </TouchableOpacity>
+        <View style={styles.settingsection}>
+          <Text style={styles.textsettings}>Pengaturan</Text>
+          <FlatList
+            data={DATA}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({item}) => (
+              <Item
+                icon={item.icon}
+                icontype={item.icontype}
+                text={item.text}
+                color={item.color}
+              />
+            )}
+          />
+        </View>
+      </View>
     </Container>
   );
 };
+
 export default Profile;
--- src/scenes/app/Profile/styles.js
+++ src/scenes/app/Profile/styles.js
@@ -3,4 +3,76 @@ import {Colors, FONTS} from 'styles';
 import {RFValue} from 'react-native-responsive-fontsize';
 const {width} = Dimensions.get('window');
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  container: {
+    backgroundColor: Colors.WHITE,
+    flex: 1,
+    marginHorizontal: width * 0.04,
+  },
+  profilesection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textprofile: {
+    flex: 1,
+  },
+  imgprofile: {
+    width: width * 0.14,
+    height: width * 0.12,
+  },
+  textname: {
+    fontFamily: FONTS.poppins[600],
+    fontSize: RFValue(21),
+    color: Colors.DRAKER_BLACK,
+  },
+  textowner: {
+    fontFamily: FONTS.poppins[500],
+    color: Colors.LIGHT_TEXT,
+    fontSize: RFValue(16),
+  },
+  premiummember: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: width * 0.17,
+    backgroundColor: '#1FB8FC1A',
+    borderRadius: width * 0.03,
+    marginVertical: width * 0.06,
+  },
+  imgpremiummember: {
+    width: width * 0.11,
+    height: width * 0.11,
+    marginHorizontal: width * 0.03,
+  },
+  textpremiummember: {
+    fontFamily: FONTS.poppins[500],
+    fontSize: RFValue(16),
+    flex: 1,
+    marginTop: width * 0.002,
+  },
+  chevronright: {
+    fontSize: RFValue(16),
+    marginHorizontal: width * 0.04,
+  },
+  iconfitur: color => ({
+    color: color,
+    fontSize: RFValue(22),
+    marginHorizontal: width * 0.03,
+  }),
+  fitursection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: width * 0.02,
+    height: width * 0.1,
+  },
+  textsettings: {
+    fontFamily: FONTS.poppins[500],
+    fontSize: RFValue(14),
+    color: Colors.LIGHT_TEXT,
+  },
+  textfitur: {
+    fontFamily: FONTS.poppins[500],
+    color: Colors.DRAKER_BLACK,
+    flex: 1,
+    fontSize: RFValue(16),
+  },
+});
